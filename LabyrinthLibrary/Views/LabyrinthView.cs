68d567839193dff82ffb6f8a59a58aad7ba79357
metin yildirim <metin_yildirim@outlook.be>
2025-09-02 09:51:25 +0200

    display labyrinth in view

--- LabyrinthLibrary/Views/LabyrinthView.cs
+++ LabyrinthLibrary/Views/LabyrinthView.cs
@@ -8,9 +8,36 @@ public class LabyrinthView
     {
         Console.WriteLine(message + $" model: {model}");
 
+        int row = 0;
+        int column = 0;
+
         foreach (var item in model)
         {
-            
+            if (item is KeyValuePair<LabyrinthPosition, ILabyrinthElement> element)
+            {
+                if (element.Key.Row > row)
+                {
+                    row++;
+                    column = 0;
+                    Console.WriteLine();
+                }
+
+                column = MoveToColumn(column, element.Key);
+
+                Console.Write(element.Value.Symbol);
+                column++;
+            }
         }
+    }
+
+    private static int MoveToColumn(int currentColumn, LabyrinthPosition position)
+    {
+        while (currentColumn / 2 < position.Column)
+        {
+            Console.Write(" ");
+            currentColumn++;
+        }
+
+        return currentColumn;
     }
 }
